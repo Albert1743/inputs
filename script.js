3b13088c4c21042577a0afe71ea77a10f116c1e9
ali <mustafaevalibek52@gmail.com>
2024-02-23 12:10:49 +0500

    inp

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-let inputs = document.querySelectorAll('.main-input, .not-main-input');
-let error = true
+const form = document.forms.signup
+const inputs = form.querySelectorAll('input')
+const errorcurr = document.querySelector('#errorcurr')
+const errortotal = document.querySelector('#errortotal')
+let countError = 0
 
 
+form.onsubmit = (event) => {
+    event.preventDefault()
+    let error = false
 
-function saveChanges() {
-    let user = {}
+    inputs.forEach(inp => {
+        let isRequired = inp.parentNode.classList.contains('req')
 
-    inputs.forEach(input => {
-        if (input.classList.contains('main-input') && !input.value.trim()) {
-            error = false
-            input.classList.add('error')
+        if (isRequired === true && inp.value.length === 0) {
+            countError++
+            error = true
+            inp.parentNode.classList.add('error')
         } else {
-            input.classList.remove('error')
+            inp.parentNode.classList.remove('error')
         }
-    });
+    })
 
-    if (error) {
-        inputs.forEach(input => {
-            user[input.name] = input.value
-        });
-        console.log(user);
-    } else {
-        console.log('Zapolni polya!');
+    
+    if (error === true) {
+        alert('Error fill all fields!')
+        return
     }
+
+    submit(event.target)
 }
+
+
+function submit(form) {
+    const fm = new FormData(form)
+    const user = {}
+
+    fm.forEach((val, key) => user[key] = val)
+
+    console.log(user);
+
+}
+
